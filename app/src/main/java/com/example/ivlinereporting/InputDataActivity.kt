2b454eac94599088be66b384aa2108385e16b92b
Artem <kudrinskiy@internet.ru>
2024-07-22 12:07:32 +0300

    Добавлено отображение нужного фрагмента на странице ввода данных в зависимости от выбора пользователя

--- app/src/main/java/com/example/ivlinereporting/InputDataActivity.kt
+++ app/src/main/java/com/example/ivlinereporting/InputDataActivity.kt
@@ -1,10 +1,12 @@
 package com.example.ivlinereporting
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class InputDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,22 @@ class InputDataActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val fragmentType = intent.getStringExtra("fragment_type")
+        val fragment = when(fragmentType){
+            "workReport" -> WorkReportFragment()
+            "workingHoursReport" -> WorkingHoursReportFragment()
+            "techniqueReport" -> TechniqueReportFragment()
+            else -> throw IllegalArgumentException("Неизвестный тип фрагмента")
+        }
+
+        showFragment(fragment)
+    }
+
+    @SuppressLint("CommitTransaction")
+    fun showFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
