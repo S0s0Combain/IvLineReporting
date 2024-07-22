826e8b126ce50ae583c4386614bb4ce70615ae37
Artem <kudrinskiy@internet.ru>
2024-07-22 15:30:34 +0300

    Добавлен класс для работы с DatePicker. Реализован функционал для поля ввода даты на странице ввода данных.

---
+++ app/src/main/java/com/example/ivlinereporting/DatePickerFragment.kt
@@ -0,0 +1,35 @@
+package com.example.ivlinereporting
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.icu.util.Calendar
+import android.os.Build
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.textfield.TextInputEditText
+
+class DatePickerFragment : DialogFragment() {
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(requireActivity(), dateSetListener, year, month, day)
+    }
+
+    @SuppressLint("DefaultLocale")
+    private val dateSetListener = DatePickerDialog.OnDateSetListener{ _, year, month, day ->
+        val selectedDate = String.format("%02d.%02d.%04d", day, month, year)
+        (activity as DatePickerDialogListener).onDateSelected(selectedDate)
+    }
+
+    interface DatePickerDialogListener{
+        fun onDateSelected(date: String)
+    }
+}
--- app/src/main/java/com/example/ivlinereporting/InputDataActivity.kt
+++ app/src/main/java/com/example/ivlinereporting/InputDataActivity.kt
@@ -1,14 +1,20 @@
 package com.example.ivlinereporting
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
 
-class InputDataActivity : AppCompatActivity() {
+
+class InputDataActivity : AppCompatActivity(), DatePickerFragment.DatePickerDialogListener {
+    lateinit var dateEditText: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,8 +31,9 @@ class InputDataActivity : AppCompatActivity() {
             "techniqueReport" -> TechniqueReportFragment()
             else -> throw IllegalArgumentException("Неизвестный тип фрагмента")
         }
-
         showFragment(fragment)
+
+        dateEditText = findViewById(R.id.dateEditText)
     }
 
     @SuppressLint("CommitTransaction")
@@ -35,5 +42,14 @@ class InputDataActivity : AppCompatActivity() {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragmentContainer, fragment)
         fragmentTransaction.commit()
+    }
+
+    fun showDatePickerDialog(v: View){
+        val datePickerFragment = DatePickerFragment()
+        datePickerFragment.show(supportFragmentManager, "datePicker")
+    }
+
+    override fun onDateSelected(date: String){
+        dateEditText.setText(date)
     }
 }
